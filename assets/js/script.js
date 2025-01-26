088ac7c1813001f7cbc7c8ade4c54042c7fc923c
Alia Jennifer <aliyajenny14@gmail.com>
2025-01-26 21:00:36 +0700

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,64 @@
 hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function() {
-    navBar = document.querySelector(".nav-bar");
-    navBar.classList.toggle("active");
-    hamburger.classList.toggle("active");
-}
+hamburger.onclick = function () {
+  navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active");
+  hamburger.classList.toggle("active");
+};
 
-const toggleButtons = document.querySelectorAll('.toggle-btn');
+const toggleButtons = document.querySelectorAll(".toggle-btn");
 
-  toggleButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      button.classList.toggle('active');
+toggleButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.classList.toggle("active");
 
-      const infoBox = button.nextElementSibling;
+    const infoBox = button.nextElementSibling;
 
-      if (infoBox.classList.contains('hidden')) {
-        infoBox.classList.remove('hidden');
-        infoBox.classList.add('visible');
-      } else {
-        infoBox.classList.add('hidden');
-        infoBox.classList.remove('visible');
-      }
-    });
+    if (infoBox.classList.contains("hidden")) {
+      infoBox.classList.remove("hidden");
+      infoBox.classList.add("visible");
+    } else {
+      infoBox.classList.add("hidden");
+      infoBox.classList.remove("visible");
+    }
   });
+});
+
+const carousel = document.getElementById("carousel");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+
+const scrollAmount = 200;
+const slideInterval = 2000;
+
+left.addEventListener("click", () => {
+  carousel.scrollBy({
+    left: -scrollAmount,
+    behavior: "smooth",
+  });
+});
+
+right.addEventListener("click", () => {
+  carousel.scrollBy({
+    left: scrollAmount,
+    behavior: "smooth",
+  });
+});
+
+let autoSlide = setInterval(() => {
+  const maxScroll = carousel.scrollWidth - carousel.clientWidth;
+
+  if (carousel.scrollLeft >= maxScroll) {
+    carousel.scrollTo({
+      left: 0,
+      behavior: "smooth",
+    });
+  } else {
+    carousel.scrollBy({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
+  }
+}, slideInterval);
+
+left.addEventListener("click", () => clearInterval(autoSlide));
+right.addEventListener("click", () => clearInterval(autoSlide));
